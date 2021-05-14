148422b8a3a2ef1d2a882774b327f1fbca54f559
William Miceli <git@williammiceli.me>
2021-05-14 15:21:49 -0400

    Copied pin definitions from Debug3 to Debug1 (this file)

--- Telem_Debug/Debug1/main.h
+++ Telem_Debug/Debug1/main.h
@@ -35,126 +35,124 @@ void timerB_init(void);
 /******************** Pin Definitions *************************/
 
 // PORT 1
-#define LED0		      	0x01
-#define LED1			    0x02
-#define BUTTON1				0x04	// BPS Reset
-#define BUTTON2 			0x08
-#define CAN_RSTn	        0x10
-#define CAN_CSn		        0x20
-#define CAN_RX0n			0x40
-#define CAN_RX1n		    0x80
-#define P1_UNUSED			0x00
+#define P10                 0x01
+#define P11                 0x02
+#define P12                 0x04
+#define P13                 0x08
+#define P14                 0x10
+#define P15                 0x20
+#define RTC_MFP             0x40
+#define IMU_INTn            0x80
+#define P1_UNUSED           0x01 | 0x02 | 0x04 | 0x08 | 0x10 | 0x20
 
 // PORT 2
-#define CAN_INTn			0x01
-#define ADC1_RDYn			0x02
-#define ADC2_RDYn			0x04
-#define ADC3_RDYn			0x08
-#define ADC4_RDYn			0x10
-#define ADC5_RDYn			0x20
-#define ADC6_RDYn			0x40
-#define ADC7_RDYn			0x80
-#define P2_UNUSED			0x00
+#define CAN0_INTn           0x01
+#define CAN0_RXB0n          0x02
+#define CAN0_RXB1n          0x04
+#define CAN1_INTn           0x08
+#define CAN1_RXB0n          0x10
+#define CAN1_RXB1n          0x20
+#define GPS_INTn            0x40
+#define P27                 0x80
+#define P2_UNUSED           0x80
 
 // PORT 3
-#define ADC_bus1_CLK		0x01
-#define CAN_SIMO			0x02
-#define CAN_SOMI			0x04
-#define CAN_SCLK			0x08
-#define ADC_bus1_MOSI		0x10
-#define ADC_bus1_MISO		0x20
-#define ADC_misc_CLK		0x40
-#define ADC_bus2_MOSI		0x80
-#define P3_UNUSED			0x00
+#define CAN0_SCLK           0x01
+#define SDC_SIMO            0x02
+#define SDC_SOMI            0x04
+#define SDC_SCLK            0x08
+#define CAN0_MOSI           0x10
+#define CAN0_MISO           0x20
+#define CAN1_CLK            0x40
+#define IMU_SDA             0x80
+#define P3_UNUSED           0x00
 
 // PORT 4
-#define ADC_CS1				0x01
-#define ADC_CS2				0x02
-#define ADC_CS3				0x04
-#define ADC_CS4				0x08
-#define ADC_CS5				0x10
-#define ADC_CS6				0x20
-#define ADC_CS7			    0x40
-#define ADC_RSTn			0x80
-#define P4_UNUSED			0x00
+#define CAN0_RSTn           0x01
+#define CAN0_CSn            0x02
+#define CAN1_RSTn           0x04
+#define CAN1_CSn            0x08
+#define SDC_WPn             0x10
+#define SDC_CDn             0x20
+#define P46                 0x40
+#define P47                 0x80
+#define P4_UNUSED           0x40 | 0x80
 
 // PORT 5
-#define P50					0x01
-#define P51					0x02
-#define XT2IN				0x04
-#define XT2OUT				0x08
-#define ADC_bus2_MISO		0x10
-#define ADC_bus2_CLK		0x20
-#define ADC_misc_MOSI	    0x40
-#define ADC_misc_MISO		0x80
-#define P5_UNUSED			0x01 | 0x02
+#define P50                 0x01
+#define P51                 0x02
+#define XT2IN               0x04
+#define XT2OUT              0x08
+#define IMU_SCL             0x10
+#define P55                 0x20
+#define CAN1_MOSI           0x40
+#define CAN1_MISO           0x80
+#define P5_UNUSED           0x01 | 0x02 | 0x20
 
 // PORT 6
-#define LED2				0x01
-#define LED3				0x02
-#define LED4				0x04
-#define LED5				0x08
-#define RELAY_BATT_OPEN		0x10
-#define RELAY_ARRAY			0x20
-#define RELAY_MCPC			0x40
-#define RELAY_MC		    0x80
-#define P6_UNUSED			0x00
+#define P60                 0x01
+#define P61                 0x02
+#define P62                 0x04
+#define P63                 0x08
+#define P64                 0x10
+#define P65                 0x20
+#define P66                 0x40
+#define P67                 0x80
+#define P6_UNUSED           0x01 | 0x02 | 0x04 | 0x08 | 0x10 | 0x20 | 0x40 | 0x80
 
 // PORT 7
-#define XT1IN				0x01
-#define XT1OUT				0x02
-#define P72					0x04
-#define P73					0x08
-#define RELAY_SCPC			0x10
-#define RELAY_SC			0x20
-#define EXT_RELAY_SCPC		0x40
-#define EXT_RELAY_MCPC		0x80
-#define P7_UNUSED			0x04 | 0x08
+#define XT1IN               0x01
+#define XT1OUT              0x02
+#define P72                 0x04
+#define P73                 0x08
+#define P74                 0x10
+#define P75                 0x20
+#define P76                 0x40
+#define P77                 0x80
+#define P7_UNUSED           0x04 | 0x08 | 0x10 | 0x20 | 0x40 | 0x80
 
 // PORT 8
-#define P80				    0x01
-#define P81			    	0x02
-#define P82			        0x04
-#define P83					0x08
-#define P84					0x10
-#define LT_CS3		    	0x20
-#define LT_CS2		    	0x40
-#define LT_CS1			    0x80
-#define P8_UNUSED			0x01 | 0x02 | 0x04 | 0x08 | 0x10
+#define P80                 0x01
+#define P81                 0x02
+#define P82                 0x04
+#define LEDG                0x08
+#define LEDR                0x10
+#define LEDY0               0x20
+#define LEDY1               0x40
+#define Button0             0x80
+#define P8_UNUSED           0x01 | 0x02 | 0x04
 
 // PORT 9
-#define LT_SCLK_1			0x01
-#define LT_MOSI_2			0x02
-#define LT_MISO_2			0x04
-#define LT_SCLK_2			0x08
-#define LT_MOSI_1			0x10
-#define LT_MISO_1			0x20
-#define P96				    0x40
-#define p97				    0x80
-#define P9_UNUSED			0x40 | 0x80
+#define P90                 0x01
+#define RTC_SDA             0x02
+#define RTC_SCL             0x04
+#define P93                 0x08
+#define USB_TX              0x10
+#define USB_RX              0x20
+#define SDC_CSn             0x40
+#define GPS_CSn             0x80
+#define P9_UNUSED           0x40 | 0x80
 
 // PORT 10
-//#define P100				0x01
-#define RELAY_BATT			0x01
-#define LT_MOSI_3			0x02
-#define LT_MISO_3			0x04
-#define LT_SCLK_3			0x08
-#define EXT_TX				0x10
-#define EXT_RX				0x20
-#define P106				0x40
-#define P107     			0x80
-//#define P10_UNUSED		0x01 | 0x40 | 0x80
-#define P10_UNUSED			0x40 | 0x80
+#define BT_CSn              0x01
+#define BT_MOSI             0x02
+#define BT_MISO             0x04
+#define BT_SCLK             0x08
+#define UART_TX             0x10
+#define UART_RX             0x20
+#define BT_EN               0x40
+#define P107                0x80
+#define P10_UNUSED          0x80
 
 // PORT 11
-#define ACLK_TEST			0x01
-#define MCLK_TEST			0x02
-#define SMCLK_TEST		   	0x04
+#define ACLK_TEST           0x01
+#define MCLK_TEST           0x02
+#define SMCLK_TEST          0x04
 
 // PORT J
-#define JTAG_TDO			0x01
-#define JTAG_TDI			0x02
-#define JTAG_TMS			0x04
-#define JTAG_TCK			0x08
+#define JTAG_TDO            0x01
+#define JTAG_TDI            0x02
+#define JTAG_TMS            0x04
+#define JTAG_TCK            0x08
 
 #endif /* MAIN_H_ */
