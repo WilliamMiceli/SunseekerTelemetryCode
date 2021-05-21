d0ef7da5c0663216b68a30e68aa0d8aff606bd18
Sunseeker Lab A-216 <brad.bazuin@wmich.edu>
2021-05-21 15:51:39 -0400

    Added more delay to be able to see LEDs toggling on and off

--- Telem_Debug/Debug_BlinkyLED-Basic/main.c
+++ Telem_Debug/Debug_BlinkyLED-Basic/main.c
@@ -17,9 +17,12 @@ int main(void) {
 
 	_EINT(); 	                                //enable global interrupts
 
+	volatile int i;
     while(1)
     {
     	P8OUT ^= LEDY0 | LEDY1;                 // Toggle both yellow LEDs
-    	delay();
+    	for(i = 1000; i > 0; i--){
+    		delay();
+    	}
     }
 }
